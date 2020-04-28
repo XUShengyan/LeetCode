d9d70e774ad9645c776d4c80047cc7f5798e8f1b
XUShengyan <edwinxsy@gmail.com>
2020-04-28 02:35:36 +0200

    twoSum

---
+++ 7Reverse Integer.py
@@ -0,0 +1,48 @@
+# Given a 32-bit signed integer, reverse digits of an integer.
+
+# Example 1:
+#
+# Input: 123
+# Output: 321
+# Example 2:
+#
+# Input: -123
+# Output: -321
+# Example 3:
+#
+# Input: 120
+# Output: 21
+###################################
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x >0:
+            floorDivision = x
+            negativFlag =0
+        else:
+            floorDivision = -x
+            negativFlag = 1
+        number = list()
+        i =0
+
+        while(floorDivision):
+            remainder = floorDivision%10
+            number.append(remainder)
+            i = i +1
+            floorDivision = floorDivision // 10
+
+        result =0
+        for i in range(i):
+            result = number.pop() * 10**i + result
+        if (2 ** -31 < result < 2 ** 31):
+            if negativFlag:
+                return -result
+            else:
+                return result
+        else:
+            return 0
+
+
+test = Solution()
+temp =test.reverse(-123)
+print(temp)
